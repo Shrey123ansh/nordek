90b5c851571b8807b8af4e361015839be941522f
issa-me-sush <coolsushaan@gmail.com>
2023-09-25 06:44:22 +0530

    social links

--- packages/nextjs/components/landingcomps/socials.tsx
+++ packages/nextjs/components/landingcomps/socials.tsx
@@ -7,7 +7,7 @@ const Socials: React.FC = () => {
   <div className="flex flex-row justify-between ">
   {
   ['/assets/discord.svg', '/assets/twitter.svg', '/assets/telegram.svg', '/assets/medium.svg', '/assets/reddit.svg'].map((iconSrc, index) => {
-    const urls = ['https://discord.com', 'https://twitter.com', 'https://telegram.org', 'https://medium.com', 'https://reddit.com'];
+    const urls = ['https://discord.com/invite/HWgDCEvR2z', 'https://twitter.com/officialnordek', 'https://t.me/officialnordek', 'https://medium.com/@officialnordek', 'https://reddit.com'];
     return (
       <div key={index} onClick={() => window.open(urls[index], '_blank')} style={{cursor: 'pointer'}} className="w-[10%]">
         <img src={iconSrc} alt={`Icon ${index + 1}`} className=" w-[100%]" />
